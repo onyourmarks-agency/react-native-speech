cc7827330e7c1ac0ae814f885c1e2c2e7301f4bd
rvgool <rvgool@onyourmarks.agency>
2025-08-21 13:37:41 +0200

    feat(audio): remove ducking module

--- ios/Speech.h
+++ ios/Speech.h
@@ -1,6 +1,5 @@
 #import "generated/RNSpeechSpec/RNSpeechSpec.h"
 #import "AVFoundation/AVFoundation.h"
-#import "Ducking.h"
 
 using namespace JS::NativeSpeech;
 
@@ -9,7 +8,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface Speech : NativeSpeechSpecBase <NativeSpeechSpec, AVSpeechSynthesizerDelegate>
 @property (nonatomic, strong) AVSpeechSynthesizer *synthesizer;
 @property (nonatomic, strong) NSDictionary *globalOptions;
-@property (nonatomic, strong) Ducking *audioDucking;
 @end
 
 NS_ASSUME_NONNULL_END
